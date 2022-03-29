321db99c8532e5bf8d3b8e21493f9fe23787469a
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-03-28 22:44:51 -0400

    Accept bin and not dec values in file

--- src/binread/bin_read.rs
+++ src/binread/bin_read.rs
@@ -5,14 +5,18 @@ use crate::instructions::InstructionSet;
 use crate::memory::InnerData;
 
 
-fn read_file_line_by_line(filepath: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+fn read_file_line_by_line(filepath: &str) -> Result<Vec<isize>, Box<dyn std::error::Error>> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
     let mut file_lines = Vec::new();
+    let mut dec_val;
 
     for line in reader.lines() {
         match line {
-            Ok(line) => file_lines.push(line),
+            Ok(line) => {
+                dec_val = isize::from_str_radix(&line, 2)?;
+                file_lines.push(dec_val)
+            },
             Err(e) => println!("Error: {}", e),
         }
     }
@@ -32,23 +36,20 @@ pub fn read_from_file(filepath: &str) -> Vec<InstructionSet> {
     let mut program = Vec::new();
     let mut is_reading_arg = false;
     let mut arg: InnerData;
-    let mut int_byte: InnerData;
 
     for byte in buffer {
         if is_reading_arg {
-            arg = byte.parse::<InnerData>().unwrap() as InnerData;
+            arg = byte as InnerData;
             let instruction = InstructionSet::from_int(0, Some(arg));
             program.push(instruction);
             is_reading_arg = false;
             continue;
         }
 
-        int_byte = byte.parse::<InnerData>().unwrap() as InnerData;
-
-        if int_byte == 0 {
+        if byte as InnerData == 0 {
             is_reading_arg = true;
         } else {
-            let instruction = InstructionSet::from_int(int_byte as u8, None);
+            let instruction = InstructionSet::from_int(byte as u8, None);
             program.push(instruction);
         }
     }
