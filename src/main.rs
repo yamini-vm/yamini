5b9778e2b1d83df05a9a8f44baef3dcb390e4c7b
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-05-21 01:01:50 -0400

    Use sargparse, add debug flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate sargparse;
+
 use std::io;
-use std::env;
+use sargparse::{ArgumentParser, ArgumentType, InnerData};
 
 use yamini::memory::DataMemory;
 use yamini::memory::Stack;
@@ -8,15 +10,29 @@ use yamini::processor::Processor;
 use yamini::binread::read_from_file;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: {} <filepath>", args[0]);
-        return;
-    }
+    let mut parser = ArgumentParser::new(Some("YamASM - Assembler for YaminiVM"));
 
-    let filepath = &args[1];
+    parser.add_argument("f", "file_path", "File path to executable binary", 
+                        true, None, ArgumentType::STR);
+    parser.add_argument("-i", "--instructions", "Flag to print compiled instructions",
+                        false, Some(InnerData::BOOL(false)), ArgumentType::BOOL);
+
+    let args = parser.parse_args().unwrap();
+
+    let filepath = &args.get("file_path").unwrap().get_str();
+    
+    let instructions_flag = args.get("instructions").unwrap().get_bool();
 
     let program = read_from_file(filepath);
+
+    if instructions_flag {
+        println!("--------------------------------------------");
+        println!("Instructions:");
+        for instruction in &program {
+            println!("{:?}", instruction);
+        }
+        println!("--------------------------------------------");
+    }
 
     let mut stack = Stack::new();
     let mut call_stack = Stack::new();
